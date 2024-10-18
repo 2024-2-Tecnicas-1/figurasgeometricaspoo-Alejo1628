f5e082d4d4dfeb4acf7d55b042ce4f535aafe005
Alejo1628 <alejogrxd@gmail.com>
2024-10-18 13:51:33 -0500

    ejercicio herencia con Figuras geométricas POO   Alejandro Galindo

---
+++ src/main/java/com/mycompany/figurasgeometricas/Circulo.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.figurasgeometricas;
+
+class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    @Override
+    public double obtenerArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
